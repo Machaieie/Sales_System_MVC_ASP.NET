20faf384f771169d93a85aa733487d8338b5ac7b
Edwin Machaieie <machaieieedwin1@gmail.com>
2023-09-24 21:59:16 +0200

    Login and others

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacao_de_vendas.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
