7f6af983f16fb55f5565a1c2aba664e65b5c4faf
Edwin Machaieie <machaieieedwin1@gmail.com>
2023-09-25 15:31:18 +0200

    finalizing the authentication and authorization class using tokens

--- Repositorys/UserRepository.cs
+++ Repositorys/UserRepository.cs
@@ -6,7 +6,16 @@ namespace Aplicacao_de_vendas.Repositorys
     {
         public static User Get(string username, string password)
         {
+            var users = new List<User>
+            {
+                new User { Id = 1, Username = "edwin", Password = "teste", Role = "manager" },
+                new User { Id = 2, Username = "leo", Password = "teste", Role = "employee" },
+            };
 
+            return users.FirstOrDefault(x =>
+                  x.Username.ToLower() == username.ToLower() && x.Password == password
+              );
         }
+
     }
 }
---
+++ Services/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Aplicacao_de_vendas.Models;
+using Aplicacao_de_vendas.Properties.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+public class TokenService
+{
+   
+
+    public string GenerateToken(User user)
+    {
+        // Gerador de tokens 
+        var tokenHandler = new JwtSecurityTokenHandler();
+        // chave 
+        var key = Encoding.ASCII.GetBytes(Settings.Secret);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Role)
+            }),
+            Expires = DateTime.UtcNow.AddHours(2), // Define o tempo de expiração do token
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        
+
+        
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    
+}
+ 
